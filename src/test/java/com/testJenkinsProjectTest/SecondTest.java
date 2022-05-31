5c1bc68ad873bfa7c29e014d25a0c14c8a07a37a
LAVA KUMAR <LAVA KUMAR@DESKTOP-FD4QRKI>
2022-05-31 10:41:13 +0530

    project2

---
+++ src/test/java/com/testJenkinsProjectTest/SecondTest.java
@@ -0,0 +1,15 @@
+package com.testJenkinsProjectTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SecondTest {
+
+	@Test
+	public void practice1Test()
+	{
+		Reporter.log("b-practice2",true);
+	
+	}
+	
+}
